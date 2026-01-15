d2d5c6c1c5c6da3dab22db3db590784a21a078b4
njoroofficial <mosesn579@gmail.com>
2026-01-15 14:32:35 +0300

    mapped objects to database tables

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,19 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+payroll.save()  # Persist to db, assign object id attribute
+print(payroll)
+
+hr = Department("Human Resources", "Building C, East Wing")
+print(hr)  
+
+hr.save()  # Persist to db, assign object id attribute
+print(hr)
+
 ipdb.set_trace()
